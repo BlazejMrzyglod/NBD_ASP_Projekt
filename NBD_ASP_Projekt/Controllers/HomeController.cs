e6ffcbc183e332743104432c4e56c75743768366
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-05-23 15:44:06 +0200

    Changed HomeController

--- NBD_ASP_Projekt/Controllers/HomeController.cs
+++ NBD_ASP_Projekt/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NBD_ASP_Projekt.Data;
+using NBD_ASP_Projekt.Models.Models;
 using NBD_ASP_Projekt.Models.ViewModels;
 using System.Diagnostics;
 
@@ -7,15 +9,33 @@ namespace NBD_ASP_Projekt.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly ComputerContext db;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, ComputerContext computerContext)
 		{
 			_logger = logger;
+			db = computerContext;
 		}
 
-		public IActionResult Index()
+		public async Task<IActionResult> Index(ComputerFilter filter)
 		{
-			return View();
+			var computers = await db.GetComputers(filter.Year, filter.ComputerName);
+			var model = new ComputerList { Computers = computers, Filter = filter };
+			return View(model);
+		}
+
+		public IActionResult Create() => View();
+
+		[HttpPost]
+		public async Task<IActionResult> Create(Computers computer)
+
+		{
+			if (ModelState.IsValid)
+			{
+				await db.Create(computer);
+				return RedirectToAction("Index");
+			}
+			return View(computer);
 		}
 
 		public IActionResult Privacy()
@@ -25,6 +45,7 @@ namespace NBD_ASP_Projekt.Controllers
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
+
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
