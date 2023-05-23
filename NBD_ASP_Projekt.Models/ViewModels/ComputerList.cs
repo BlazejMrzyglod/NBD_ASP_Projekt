d2e96d48b6dc14c899a53881f5cfb8daa691bac7
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-05-23 15:45:09 +0200

    Changed location of ComputerList model

---
+++ NBD_ASP_Projekt.Models/ViewModels/ComputerList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NBD_ASP_Projekt.Models.Models;
+
+namespace NBD_ASP_Projekt.Models.ViewModels
+{
+    public class ComputerList
+    {
+        public IEnumerable<Computers> Computers { get; set; }
+        public ComputerFilter Filter { get; set; }
+    }
+}
