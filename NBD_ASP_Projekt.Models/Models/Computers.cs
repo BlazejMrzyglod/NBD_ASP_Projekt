25c46d7a686a5a90ffb5d34c190cc2c97b5cff0c
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-05-23 15:32:40 +0200

    Added Computers model

---
+++ NBD_ASP_Projekt.Models/Models/Computers.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBD_ASP_Projekt.Models.Models
+{
+	public class Computers
+	{
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string? Id { get; set; }
+
+		[DisplayName("Computer name")]
+		public string? Name { get; set; }
+
+		[DisplayName("Creation year")]
+		public int? Year { get; set; }
+
+		public string? ImageId { get; set; }
+		public bool HasImage() => !string.IsNullOrWhiteSpace(ImageId);
+	}
+}
