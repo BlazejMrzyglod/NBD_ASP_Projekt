895ffbaa71b3c2fdb74a77818ffb8859fe19254f
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-05-23 15:41:24 +0200

    Added COmputerFilter and ComputerList models

---
+++ NBD_ASP_Projekt.Models/Models/ComputerFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBD_ASP_Projekt.Models.Models
+{
+	public class ComputerFilter
+	{
+        public string? ComputerName { get; set; }
+        public int? Year { get; set; }
+    }
+}
