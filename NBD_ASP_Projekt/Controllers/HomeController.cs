c234943edce7c8efba547a62e25c446dc57a87e7
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-05-30 15:51:27 +0200

    Update HomeController.cs

--- NBD_ASP_Projekt/Controllers/HomeController.cs
+++ NBD_ASP_Projekt/Controllers/HomeController.cs
@@ -4,50 +4,98 @@ using NBD_ASP_Projekt.Models.ViewModels;
 using NBD_ASP_Projekt.Services;
 using System.Diagnostics;
 
-namespace NBD_ASP_Projekt.Controllers
+namespace Lab8.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
-		private readonly ComputerContext db;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ComputerContext db;
 
-		public HomeController(ILogger<HomeController> logger, ComputerContext computerContext)
-		{
-			_logger = logger;
-			db = computerContext;
-		}
+        public HomeController(ILogger<HomeController> logger, ComputerContext computerContext)
+        {
+            _logger = logger;
+            db = computerContext;
+        }
 
-		public async Task<IActionResult> Index(ComputerFilter filter)
-		{
-			var computers = await db.GetComputers(filter.Year, filter.ComputerName);
-			var model = new ComputerList { Computers = computers, Filter = filter };
-			return View(model);
-		}
+        public async Task<IActionResult> Index(ComputerFilter filter)
+        {
+            var computers = await db.GetComputers(filter.Year, filter.ComputerName);
+            var model = new ComputerList { Computers = computers, Filter = filter };
+            return View(model);
+        }
 
-		public IActionResult Create() => View();
+        public IActionResult Create() => View();
 
-		[HttpPost]
-		public async Task<IActionResult> Create(Computers computer)
+        [HttpPost]
+        public async Task<IActionResult> Create(Computers computer)
+        {
+            if (ModelState.IsValid)
+            {
+                await db.Create(computer);
+                return RedirectToAction("Index");
+            }
+            return View(computer);
+        }
 
-		{
-			if (ModelState.IsValid)
-			{
-				await db.Create(computer);
-				return RedirectToAction("Index");
-			}
-			return View(computer);
-		}
+        public async Task<IActionResult> Edit(string id)
+        {
+            Computers computer = await db.GetComputer(id);
+            if (computer == null)
+                return Error();
+            else
+                return View(computer);
+        }
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+        [HttpPost]
+        public async Task<IActionResult> Edit(Computers computer)
+        {
+            if (ModelState.IsValid)
+            {
+                await db.Update(computer);
+                return RedirectToAction("Index");
+            }
+            return View(computer);
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
+        public async Task<IActionResult> Delete(string id)
+        {
+            await db.Remove(id);
+            return RedirectToAction("Index");
+        }
 
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        public async Task<IActionResult> AttachImage(string id)
+        {
+            Computers computer = await db.GetComputer(id);
+            if (computer == null)
+                return Error();
+            else
+                return View(computer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AttachImage(string id, IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+                await db.StoreImage(id, uploadedFile.OpenReadStream(), uploadedFile.FileName);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> GetImage(string id)
+        {
+            var image = await db.GetImage(id);
+            if (image == null) return Error();
+            return File(image, "image/png");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
